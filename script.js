8340ae13336cf2f703584670e8445962bc13ab56
Caldwell <99009777+justinpse@users.noreply.github.com>
2024-01-26 13:10:47 +0800

    Add game function that loops playRound five times and gets input from user each time.

--- script.js
+++ script.js
@@ -40,6 +40,13 @@ function playRound(playerSelection, computerSelection){
 
 }
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+function game(){
+    // loop five times to play best-of-five game
+    
+    for (let i = 0; i < 5; i++) {
+        let playerSelection = prompt("Rock, paper, or scissors?");
+        const computerSelection = getComputerChoice();
+        console.log(playRound(playerSelection, computerSelection));
+    }
+
+}
