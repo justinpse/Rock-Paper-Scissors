f83f5c13ee100dc4e1a8cc7a5f17fa801cca12ce
Caldwell <99009777+justinpse@users.noreply.github.com>
2024-01-26 13:06:29 +0800

    Expand conditional statement in playRound to account for other situations (e.g. paper v scissors, etc.)

--- script.js
+++ script.js
@@ -24,7 +24,19 @@ function playRound(playerSelection, computerSelection){
 
     if (playerSelection == "rock" && computerSelection == "paper") {
         return "You lose! Paper beats rock.";
-    };
+    } else if (playerSelection == "paper" && computerSelection == "rock") {
+        return "You win! Paper beats rock.";
+    } else if (playerSelection == "paper" && computerSelection == "scissors") {
+        return "You lose! Scissors beats paper.";
+    } else if (playerSelection == "scissors" && computerSelection == "paper") {
+        return "You win! Scissors beats paper.";
+    } else if (playerSelection == "scissors" && computerSelection == "rock") {
+        return "You lose! Rock beats scissors.";
+    } else if (playerSelection == "rock" && computerSelection == "scissors") {
+        return "You win! Rock beats scissors.";
+    } else {
+        return playRound(playerSelection, getComputerChoice());
+    }
 
 }
 
